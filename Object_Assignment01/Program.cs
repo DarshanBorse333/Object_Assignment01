f98968145051e6c80277fcb6d89aae07a360fcd4
DarshanBorse333 <Darshanborse@gmail.com>
2023-03-03 17:09:48 +0530

    Add project files.

---
+++ Object_Assignment01/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Object_Assignment01;
+using System.ComponentModel.DataAnnotations;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Student1 object information");
+        Student student1 = new Student();
+        student1.GetData();
+        student1.PutData();
+
+        Console.WriteLine("Student2 object information");
+        Student student2 = new Student();
+        student2.GetData();
+        student2.PutData();
+
+        Console.WriteLine("Student3 object information");
+        Student student3 = new Student();
+        student3.GetData();
+        student3.PutData();
+
+    }
+}
+
---
+++ Object_Assignment01/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Assignment01
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string City { get; set; }
+
+        public void GetData()
+        {
+            Console.WriteLine("Please enter the value for Student Id");
+            this.Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value for Student Name");
+            this.Name = Console.ReadLine();
+            Console.WriteLine("Please enter the value for Student Gender");
+            this.Gender = Console.ReadLine();
+            Console.WriteLine("Please enter the value for Student Age");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value for Student City");
+            this.City = Console.ReadLine();
+     
+        }
+
+        public void PutData()
+        {
+            Console.WriteLine("Id = " +this.Id);
+            Console.WriteLine("Name = " + this.Name);
+            Console.WriteLine("Gender = " + this.Gender);
+            Console.WriteLine("Age = " + this.Age);
+            Console.WriteLine("City = " + this.City);
+        }
+    }
+}
